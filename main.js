9672dc7f0e05d340da6c8bdcf704520a3997c201
lakshya736 <83813486+lakshya736@users.noreply.github.com>
2021-11-24 18:55:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keypressed = e.keycode;
+    console.log(keypressed);
+
+    if((keypressed >=97 && keypressed<=122 (keypressed >=65 && keypressed<=90))
+    
+        alpha betkey();
+        document.getElementById(d1).innerHTML="You pressed alphabet Key";
+        console.log(alphabet key);
+    }
+
+    function alphabetkey()
+    {
+        img_image="Alpkey.png"
+        addEventListener();
+    }
+
